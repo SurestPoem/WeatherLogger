1ad91c997b65a1300e18d116bc1fe2641a5d6149
SurestPoem <hoth-yoda@outlook.com>
2025-07-17 01:39:24 +0100

    Update weather_logger.py

--- Weather_Logger/weather_logger.py
+++ Weather_Logger/weather_logger.py
@@ -3,6 +3,8 @@ import csv
 import os
 from collections import deque
 import time
+from datetime import datetime
+from zoneinfo import ZoneInfo  # Python 3.9+
 
 start = time.perf_counter()
 
@@ -80,6 +82,7 @@ def get_current_weather(latitude, longitude):
     }
 
     print("Weather data fetched successfully:", weather_data)
+    print("Exact time of data fetch:", datetime.now(ZoneInfo("Europe/London")).strftime("%Y-%m-%d %H:%M:%S"))
 
     return weather_data
 
